22a06706ce32cf7c7d91981a6faa1c81c5b982df
John Yorke <yorkejohn@yahoo.com>
2020-01-12 13:50:22 -0400

    Update version info

--- src/main/java/yorkejohn/tfcmetric/common/lib/LibInfo.java
+++ src/main/java/yorkejohn/tfcmetric/common/lib/LibInfo.java
@@ -4,6 +4,6 @@ public class LibInfo
 {
 	public static final String MODID = "tfcmetric";
 	public static final String NAME = "TFC Metric";
-	public static final String VERSION = "r1.0";
+	public static final String VERSION = "r1.1";
 	public static final String DEPENDENCIES = "required-after:terrafirmacraft";
 }
