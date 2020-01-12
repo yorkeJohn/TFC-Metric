29bb628a01d45d4da25e1d5e47a819757835719e
John Yorke <yorkejohn@yahoo.com>
2020-01-12 13:48:53 -0400

    Made it work for meals and milk

--- src/main/java/yorkejohn/tfcmetric/common/handler/MetricHandler.java
+++ src/main/java/yorkejohn/tfcmetric/common/handler/MetricHandler.java
@@ -6,8 +6,9 @@ import java.util.List;
 
 import com.bioxx.tfc.Core.TFC_Core;
 import com.bioxx.tfc.Food.ItemFoodTFC;
+import com.bioxx.tfc.Food.ItemMeal;
+import com.bioxx.tfc.Items.Tools.ItemCustomBucketMilk;
 import com.bioxx.tfc.api.Food;
-import com.bioxx.tfc.api.Util.Helper;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.item.ItemStack;
@@ -21,12 +22,12 @@ public class MetricHandler
 		ItemStack s = e.itemStack;
 		List<String> l = e.toolTip;
 
-		if (s != null && s.getItem() instanceof ItemFoodTFC)
+		if (s != null && (s.getItem() instanceof ItemFoodTFC || s.getItem() instanceof ItemMeal || s.getItem() instanceof ItemCustomBucketMilk))
 		{
 			for (int idx = 0; idx < l.size(); idx++)
 			{
 				String str = l.get(idx);
-				if (str.contains(TFC_Core.translate("gui.food.amount")))
+				if (str.contains("oz"))
 				{
 					l.remove(idx);
 					float oz = Food.getWeight(s);
@@ -34,10 +35,10 @@ public class MetricHandler
 					BigDecimal bd = new BigDecimal(grams);
 					bd = bd.round(new MathContext(5));
 					grams = bd.floatValue();
-					if (grams > 0.0F && grams <= 1000.0F)
-					{
+					if (!(s.getItem() instanceof ItemCustomBucketMilk) && grams > 0.0F && grams <= 1000.0F)
 						l.add(idx, TFC_Core.translate("gui.food.amount") + " " + grams + " g / " + 1000.0F + " g");
-					}
+					else if(grams > 0.0F)
+						l.add(idx, grams + " g");
 				}
 			}
 		}
